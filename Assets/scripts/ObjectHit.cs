97f15187d749c866da497219f167c893a738de1f
Anubhab Karki <bhubhik@gmail.com>
2021-10-30 12:08:07 +1100

    Blindspot

--- Assets/scripts/ObjectHit.cs
+++ Assets/scripts/ObjectHit.cs
@@ -5,6 +5,9 @@ using UnityEngine;
 public class ObjectHit : MonoBehaviour
 {
 private void OnCollisionEnter(Collision other) {
+    if(other.gameObject.tag == "Player"){
     GetComponent<MeshRenderer>().material.color = Color.red;
+    gameObject.tag = "Hit";
+    }
 }
 }
--- Assets/scripts/Scorer.cs
+++ Assets/scripts/Scorer.cs
@@ -6,7 +6,9 @@ public class Scorer : MonoBehaviour
 {
     int hits = 0;
     private void OnCollisionEnter(Collision other) {
-        hits++;
-        Debug.Log("You've hit the walls " + hits +  " times");
+        if(other.gameObject.tag != "Hit"){
+             hits++;
+            Debug.Log("You've hit the walls " + hits +  " times");
+        }
     }
 }
